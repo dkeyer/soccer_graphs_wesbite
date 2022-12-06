13859dca0705ec3255f8a68b59b24fc657f98292
dkeyer <dkeyer@gmail.com>
2022-12-06 14:07:11 -0500

    setting up contact form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,22 @@
 export default function Contact() {
-    return <h1>Contact</h1>
+    return (
+      <body>
+        <form action="contactInfo.tsx" method="GET">
+<div>
+  <label htmlFor="name">Name</label>
+  <input type="text" name="name" id="name" required/>
+</div>
+<div>
+  <label htmlFor="email">Email</label>
+  <input type="email" name="email" id="email" required/>
+</div>
+<div>
+  <label htmlFor="message">Message</label>
+  <input type="text" name="message" id="message" required/>
+</div>
+<button type="submit">Submit</button>
+</form>
+</body>
+
+) 
   }
